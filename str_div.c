3a6c0859775f0979da50e6eb7ca8d4bbc87e53e9
domgeorge <obinnadom@gmail.com>
2023-12-13 01:22:21 -0800

    Handled error

--- str_div.c
+++ str_div.c
@@ -3,24 +3,41 @@
 void str_div(char *str);
 
 /**
-* str_div - splits the strings
-* @str: input for string to be splited
-*/
+ * str_div - Splits the strings.
+ * @str: Input for the string to be split.
+ */
 void str_div(char *str)
 {
-char *token, *cut_str = str;
+char *token, *cut_str = str, *end;
 char *delimiter = " \t\n";
 char **array_token = malloc(sizeof(char *) * ULT_CHAR);
-int count = 0;
-int i;
+int count = 0, envy;
+char *temp;
 
+for (; *cut_str == ' ' || *cut_str == '\t'; cut_str++)
+{
+}
+end = cut_str + _strlen(cut_str) - 1;
+for (; end > cut_str && (*end == ' ' || *end == '\t'); end--)
+{
+*end = '\0';
+}
 if (!array_token)
 {
-perror("Memory allocation failed");
+perror(" not available");
 exit(EXIT_FAILURE);
 }
+temp = _strtok(cut_str, delimiter);
 
-while ((token = _strtok(cut_str, delimiter)) != NULL)
+if (temp != NULL)
+{
+token = temp;
+}
+else
+{
+token = NULL;
+}
+while (token != NULL)
 {
 array_token[count] = _strdup(token);
 if (array_token[count] == NULL)
@@ -29,11 +46,18 @@ perror("String tokenization failed");
 exit(EXIT_FAILURE);
 }
 count++;
-cut_str = NULL;
+temp = _strtok(NULL, delimiter);
+
+if (temp != NULL)
+{
+token = temp;
 }
-
+else
+{
+token = NULL;
+}
+}
 array_token[count] = NULL;
-
 if (count > 0)
 {
 if (!inbuilt(array_token))
@@ -41,10 +65,9 @@ if (!inbuilt(array_token))
 execute(array_token);
 }
 }
-
-for (i = 0; i < count; i++)
+for (envy = 0; envy < count; envy++)
 {
-free(array_token[i]);
+free(array_token[envy]);
 }
 free(array_token);
 }
